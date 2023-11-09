6bff9fc2557860e5d202b956fab22278847bb863
conte <gabriel_colet@hotmail.com>
2023-11-09 12:02:06 -0300

    toma entao vai tomando

--- main.py
+++ main.py
@@ -8,7 +8,9 @@ from aux_file import priority_directions
 from environment import Env
 from explorer_robot import ExplorerRobot
 from rescuer import Rescuer
-
+import pickle
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
+import pandas as pd
 
 def main(data_folder_name):
     # Set the path to config files and data files for the environment
@@ -42,8 +44,18 @@ def main(data_folder_name):
     # Run the environment simulator
     env.run()
 
+    df = pd.DataFrame(columns=['id', 'qPA', 'pulso', 'freq_resp'])
+    posistions_array_x = []
+    posistions_array_y = []
+    pos_array = []
+
+
+    filename = "arvore.pkl"
+    arvore = pickle.load(open(filename, 'rb'))
+
     for id, data in exp1.victims.items():
         all_victims[id] = data
+        print(data.resp)
 
     for id, data in exp2.victims.items():
         all_victims[id] = data
@@ -55,8 +67,40 @@ def main(data_folder_name):
         all_victims[id] = data
     
     for id, data in all_victims.items():
-        print(f"id: {id}, pulse: {data.pulse}")
+        print(f"id: {id}, qpa: {data.qPA}, pulse: {data.pulse}, resp: {data.resp}")
 
+        pos_array.append(data.pos)
+        posistions_array_x.append(data.pos[0])
+        posistions_array_y.append(data.pos[1])
+
+        new_row = {'id': data.id, 'qPA': data.qPA, 'pulso': data.pulse, 'freq_resp': data.resp}
+        df = df.append(new_row, ignore_index=True)
+    print(type(all_victims))
+    print(df.head())
+    df['classe'] = arvore.predict(df)
+    df['grav'] = 0
+    #df['x'] = posistions_array_x  caso você queira as posicoes no df em x,y ao inves de tuple
+    #df['y'] = posistions_array_y   é só desfazer esses comentários
+    df['pos'] = pos_array
+
+    clean = ["id", "pos", "grav", "classe"] # eai aqui trocar "pos" por "x", "y"
+    clean_df = df[clean]
+    print(clean_df.head())
+    clean_df.to_csv('resultados.csv', index=False)
+
+
+    #print(df.head())
+
+
+
+
+
+    #df = pd.DataFrame(all_victims)
+    #df = df.T
+    #print(df.head())
+    #filename = "arvore.pkl"
+    #arvere = pickle.load(open(filename, 'rb'))
+    #print(all_victims)
 if __name__ == '__main__':
     """ To get data from a different folder than the default called data
     pass it by the argument line"""
